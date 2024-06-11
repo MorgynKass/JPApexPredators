981ee9c9edbdbd26e4ac5c5ee764d15cd6a854dc
MorgynKass <morgynkass@gmail.com>
2024-06-11 13:01:59 -0600

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Morgyn Peay on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
